8ecb69603901ca78155dd4a3c2e40035a4f90dcf
Tony Dao <sangdao2014@csu.fullerton.edu>
2015-05-01 15:26:01 -0700

    Fixed the problem and add condition for for the word in table

--- main_dd.cpp
+++ main_dd.cpp
@@ -47,35 +47,32 @@ int main()
         //cout << "line # : " << line << endl;
 		file >> Sample >> BgL >> RelTime >> AbsTime >> Transfer >> AMXAM >> Address >> Data >> Size >> Cycle;
         getline(file, test_line);
-
+		test_line = "";
         //cout << test_line << endl << endl;
         
         //use substr to parse portion of a string
-		parsed_info.Address = Address;
+		/*parsed_info.Address = Address;
         parsed_info.Data = Data;
         parsed_info.Size = Size;
-        parsed_info.Cycle = Cycle;
-
-		left_4bit = parsed_info.Data.substr(0,4);
-        right_4bit = parsed_info.Data.substr(4,8);
-
+        parsed_info.Cycle = Cycle;*/
+		//cout << "Line " << line << ":[" <<AbsTime << "]["  << Transfer << "]["  << AMXAM << "]["  << Address << "]["  << Data << "]["  << Cycle << "][" << test_line << "]"<< endl;
         //check length for S-D
-        if (parsed_info.Address == "40000810")
+        if (Address == "40000810")
         {
             std::cout << "Line " << line << ": ";
             
-            if(parsed_info.Cycle == "Wr")
+            if(Cycle == "Wr")
             {
-                parsed_info.Cycle = "Write";
+                Cycle = "Write";
             }
             else
             {
-                parsed_info.Cycle = "Read";
+                Cycle = "Read";
             }
             
-            num_of_word = convertHexToInt(parsed_info);
+			num_of_word = (stoi(Data, nullptr, 16))/2;
             
-            std::cout << parsed_info.Cycle << " S-D command " << convertHexToInt(parsed_info) << " words" << endl;
+            std::cout << Cycle << " S-D command " << num_of_word << " words" << endl;
                         
             if (num_of_word > 0)
             {
@@ -85,23 +82,23 @@ int main()
 				cout << endl;
             
         }
-        else if(parsed_info.Address == "40000C18")
-        {            
+        else if(Address == "40000C18")
+        { 
             std::cout << "Line " << line << ": ";            
             
             //check Data amount at address 
-			if(parsed_info.Cycle == "Wr")
+			if(Cycle == "Wr")
             {
-                parsed_info.Cycle = "Write";
+                Cycle = "Write";
             }
             else
             {
-                parsed_info.Cycle = "Read";
+                Cycle = "Read";
             }
             
-            num_of_word = convertHexToInt(parsed_info);
+			num_of_word = (stoi(Data, nullptr, 16))/ 2;
             
-            std::cout << parsed_info.Cycle << " D-S command " << convertHexToInt(parsed_info) << " words" << endl;
+            std::cout << Cycle << " D-S command " << num_of_word << " words" << endl;
             
             if (num_of_word > 0)
             {
@@ -112,26 +109,40 @@ int main()
         }
         else if (s_to_d)
         {
+			left_4bit = Data.substr(0,4);
+			right_4bit = Data.substr(4,8);
 			num_of_word -= 2;
-			if(num_of_word > 1 && parsed_info.Address == "40000818")
+			if(num_of_word > 1 && Address == "40000818")
 			{
 				increase = true;
 			}
-			start_word = stoi(parsed_info.Address.substr(5,3), nullptr, 16) - 2072;
+			start_word = stoi(Address.substr(5,3), nullptr, 16) - 2072;
 			start_word /= 2;
             if(increase)
             {
+				if(start_word == 0 || start_word == 1 || start_word == 4 || start_word == 5 || start_word == 10 || start_word == 15 || start_word == 22 || start_word == 32 || start_word == 37 || start_word == 38 || start_word == 40 || start_word == 41)
+				{
 				convert_4_bits StoD_left_4(left_4bit, start_word);
                 std::cout << "Line " << line << ": Word " << start_word << ": " << StoD_left_4.get_value() << endl;
+				}
+				if(start_word+1 == 0 || start_word+1 == 1 || start_word+1 == 4 || start_word+1 == 5 || start_word+1 == 10 || start_word+1 == 15 || start_word+1 == 22 || start_word+1 == 32 || start_word+1 == 37 || start_word+1 == 38 || start_word+1 == 40 || start_word+1 == 41)
+				{
 				convert_4_bits StoD_right_4(right_4bit, start_word+1);
                 std::cout << "Line " << line << ": Word " << start_word+1 << ": " << StoD_right_4.get_value() << endl;
+				}
             }
             else
             {
+				if(start_word+1 == 0 || start_word+1 == 1 || start_word+1 == 4 || start_word+1 == 5 || start_word+1 == 10 || start_word+1 == 15 || start_word+1 == 22 || start_word+1 == 32 || start_word+1 == 37 || start_word+1 == 38 || start_word+1 == 40 || start_word+1 == 41)
+				{
 				convert_4_bits StoD_right_4(right_4bit, start_word+1);
                 std::cout << "Line " << line << ": Word " << start_word+1 << ": " << StoD_right_4.get_value() << endl;
+				}
+				if(start_word == 0 || start_word == 1 || start_word == 4 || start_word == 5 || start_word == 10 || start_word == 15 || start_word == 22 || start_word == 32 || start_word == 37 || start_word == 38 || start_word == 40 || start_word == 41)
+				{
 				convert_4_bits StoD_left_4(left_4bit, start_word);
                 std::cout << "Line " << line << ": Word " << start_word << ": " << StoD_left_4.get_value() << endl;
+				}
             }            
 
             if (num_of_word <= 0)
@@ -143,38 +154,52 @@ int main()
         }
         else if (d_to_s)
         {
+			left_4bit = Data.substr(0,4);
+			right_4bit = Data.substr(4,8);
 			num_of_word -= 2;
-			if(num_of_word > 1 && parsed_info.Address == "40000C18")
+			if(num_of_word > 1 && Address == "40000C18")
 			{
 				increase = true;
 			}
 
-			start_word = stoi(parsed_info.Address.substr(5,3), nullptr, 16) - 3104;
+			start_word = stoi(Address.substr(5,3), nullptr, 16) - 3104;
 			start_word /= 2;
             if(increase)
             {
+				if(start_word == 0 || start_word == 1 || start_word == 4 || start_word == 5 || start_word == 10 || start_word == 15 || start_word == 22 || start_word == 32 || start_word == 37 || start_word == 38 || start_word == 40 || start_word == 41)
+				{
 				convert_4_bits DtoS_left_4(left_4bit, start_word);
                 std::cout << "Line " << line << ": Word " << start_word << ": " << DtoS_left_4.get_value() << endl;
+				}
+				if(start_word+1 == 0 || start_word+1 == 1 || start_word+1 == 4 || start_word+1 == 5 || start_word+1 == 10 || start_word+1 == 15 || start_word+1 == 22 || start_word+1 == 32 || start_word+1 == 37 || start_word+1 == 38 || start_word+1 == 40 || start_word+1 == 41)
+				{
 				convert_4_bits DtoS_right_4(right_4bit, start_word+1);
                 std::cout << "Line " << line << ": Word " << start_word+1 << ": " << DtoS_right_4.get_value() << endl;
+				}
             }
             else
             {
+				if(start_word+1 == 0 || start_word+1 == 1 || start_word+1 == 4 || start_word+1 == 5 || start_word+1 == 10 || start_word+1 == 15 || start_word+1 == 22 || start_word+1 == 32 || start_word+1 == 37 || start_word+1 == 38 || start_word+1 == 40 || start_word+1 == 41)
+				{
 				convert_4_bits DtoS_right_4(right_4bit, start_word+1);
                 std::cout << "Line " << line << ": Word " << start_word+1 << ": " << DtoS_right_4.get_value() << endl;
+				}
+				if(start_word == 0 || start_word == 1 || start_word == 4 || start_word == 5 || start_word == 10 || start_word == 15 || start_word == 22 || start_word == 32 || start_word == 37 || start_word == 38 || start_word == 40 || start_word == 41)
+				{
 				convert_4_bits DtoS_left_4(left_4bit, start_word);
                 std::cout << "Line " << line << ": Word " << start_word << ": " << DtoS_left_4.get_value() << endl;
+				}
             }
 
             if (num_of_word <= 0)
             {
-                s_to_d = false;
+                d_to_s = false;
                 increase = false;
 				cout << endl;
             }
         }
         line++;
-        if(line > 800)
+        if(line > 1100)
         {
             break;
         }
@@ -186,13 +211,13 @@ int main()
 }
 
 
-int convertHexToInt(Record parsed_info)
+int convertHexToInt(string data)
 {
     //code sourced from stackoverflow    
     unsigned int x;
     stringstream ss;
 
-    ss << std::hex << parsed_info.Data;
+    ss << std::hex << data;
     ss >> x;
     
     //return x/2 for word count
